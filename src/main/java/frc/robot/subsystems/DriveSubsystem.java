c9a6360ece51dba9cb52b456cfa1f7624052488c
WCSZLIT <zlacy@wcs.k12.in.us>
2026-02-27 14:06:35 -0500

    UNTESTED - faster swerver and Field orient

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -273,9 +273,11 @@ public class DriveSubsystem extends SubsystemBase {
     double rotDelivered = rot * DriveConstants.kMaxAngularSpeed * RobotContainer.speedMode;
 
     // Build chassis speeds in correct units and honor field-relative flag
-    ChassisSpeeds chassisSpeeds = fieldRelative
-        ? ChassisSpeeds.fromFieldRelativeSpeeds(xSpeedDelivered, ySpeedDelivered, rotDelivered, m_Pigeon2.getRotation2d())
-        : new ChassisSpeeds(xSpeedDelivered, ySpeedDelivered, rotDelivered);
+  // Use the same gyro sign convention as odometry (odometry negates the pigeon yaw).
+  var robotRotation = Rotation2d.fromDegrees(-m_Pigeon2.getYaw().getValueAsDouble());
+  ChassisSpeeds chassisSpeeds = fieldRelative
+    ? ChassisSpeeds.fromFieldRelativeSpeeds(xSpeedDelivered, ySpeedDelivered, rotDelivered, robotRotation)
+    : new ChassisSpeeds(xSpeedDelivered, ySpeedDelivered, rotDelivered);
 
     var swerveModuleStates = DriveConstants.kDriveKinematics.toSwerveModuleStates(
         ChassisSpeeds.discretize(chassisSpeeds, periodSeconds));
